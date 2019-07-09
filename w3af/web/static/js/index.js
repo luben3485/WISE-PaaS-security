f72e880b3272b10f9b54324047d04ed667f66a19
luben3485 <luben3485@gmail.com>
2019-07-09 17:31:07 +0800

    add SSO service

--- w3af/web/static/js/index.js
+++ w3af/web/static/js/index.js
@@ -1,7 +1,82 @@
 $(document).ready(function(){
-    
-	var idi = 0;
+    var id = 0;
 	var message;
+    var myUrl = window.location.protocol + '//' + window.location.hostname;
+    var ssoUrl = 'https://portal-sso.wise-paas.io';
+    $.ajax({
+        url: ssoUrl + '/v2.0/users/me',
+        method: 'GET',
+        xhrFields: {
+            withCredentials: true
+        }
+    }).done(function (user) {
+        alert('Hello! ' + user.lastName + ' ' + user.firstName);
+    }).fail(function () {
+        forcelogin();
+        alert('You are not logged in!');
+    });
+    
+    
+    
+    
+    function forcelogin() {
+    
+        $('.ui.basic.modal')
+            .modal({
+            closable  : false,
+            onDeny    : function(){
+                alert("login!!");
+        
+        /*
+        $.ajax({
+        url: 'https://portal-sso.wise-paas.io/v2.0/auth/native',
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin":"*"
+        },
+        data: {
+            "username":"Ben.Lu@advantech.com.tw",
+            "password":"@Aa123456"    
+        },
+        xhrFields: {
+            withCredentials: true
+        }
+    }).done(function (res) {
+        alert(res);
+    }).fail(function (jqXHR, textStatus, errorThrown) {
+        alert(textStatus+":)");
+    });
+        
+        */
+
+        /*
+        var d = new Date();
+        d.setTime(d.getTime()+(60*60*1000));
+        var expires = "expires="+d.toGMTString();
+        cvalue = "tokenbrabrabra~~~"
+        document.cookie = "EIToken" + "=" + cvalue + "; "// + expires+";domain=.wise-paas.io; path=/";
+
+        alert(document.cookie);
+        */
+        
+        
+                return false;
+            },
+            onApprove : function() {
+                //window.alert('Approved!');
+                //window.location.href = ssoUrl + '/web/signIn.html?redirectUri=' + myUrl;
+                window.location.href = ssoUrl + '/web/signIn.html?redirectUri=' + myUrl;
+        
+            }
+            })
+    .modal('show');
+    
+    
+    }
+    
+    
+
 	$('#startScan').click(function(){
       $.ajax({
                 url: '/startScan',
