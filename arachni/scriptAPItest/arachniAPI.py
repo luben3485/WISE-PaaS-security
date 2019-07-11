83e59fa54a3377f897b81edafcf1eb1545475607
luben3485 <luben3485@gmail.com>
2019-07-11 14:27:46 +0800

    script for calling arachni API

---
+++ arachni/scriptAPItest/arachniAPI.py
@@ -0,0 +1,51 @@
+import argparse
+import requests
+import json
+
+def process_command():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--address', '-a', type=str, required=False, help='API server address',default="127.0.0.1:5000")
+	parser.add_argument('--target', '-t', type=str, required=False, help='target URL',default="http://testphp.vulnweb.com")
+	parser.add_argument('--scanmethod', '-m', type=str, required=True, help='scan methond')
+	parser.add_argument('--id', '-id', type=str, required=True, help='id number')
+	return parser.parse_args()
+if __name__ == '__main__':
+	args = process_command()
+	if args.scanmethod == "start":
+		
+		data = {
+			"url" : args.target,
+			"checks": [
+				"*"
+			],
+		"audit": {
+			"forms": True,
+			"cookies": False,  
+			"headers": False
+		},
+		"scope": {
+			"page_limit": 5,
+			"exclude_path_patterns": [
+				"logout",
+				"security",
+				"login",
+				"setup"
+			]  
+		}
+		}
+		
+		print(json.dumps(data))
+		response = requests.post('http://' + args.address  + '/scans',data=json.dumps(data),headers={'content-type': 'application/json'})
+		print(response.text)
+		#response = response.json()
+		#print(response)
+	elif args.scanmethod == "result":
+		response = requests.get('http://' + args.address  + '/scans/'+args.id+'/report')
+		print(response.text)
+
+ 
+
+
+
+
+
