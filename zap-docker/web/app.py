d4ce34e7111958909935354dfdd00d91e4071a98
luben3485 <luben3485@gmail.com>
2019-08-01 15:02:36 +0800

    checkScan

--- zap-docker/web/app.py
+++ zap-docker/web/app.py
@@ -748,25 +748,24 @@ def checkScan():
 			print(scanOption)
 			print(spiderId)
 			
-			if scanOption == 0:
+			if scanOption == '0':
 				print('test')
-				#r = requests.get('http://127.0.0.1:5000/JSON/spider/view/scans/?')	
-				#r = r.json()
-				#scanList = r['scans']
-				#for scan in scanList:
-				#	if scan['id'] == spiderId:
-				result = {'Result':'OK'}
-				return jsonify(result)		
-				#result = {'Result':'NO'}
-				#return jsonify(result)
-				
-			elif scanOption == 2:
 				r = requests.get('http://127.0.0.1:5000/JSON/spider/view/scans/?')	
 				r = r.json()
 				scanList = r['scans']
 				for scan in scanList:
 					if scan['id'] == spiderId:
-						finish = 1
+						result = {'Result':'OK'}
+						return jsonify(result)		
+				result = {'Result':'NO'}
+				return jsonify(result)
+				
+			elif scanOption == '2':
+				r = requests.get('http://127.0.0.1:5000/JSON/spider/view/scans/?')	
+				r = r.json()
+				scanList = r['scans']
+				for scan in scanList:
+					if scan['id'] == spiderId:
 						result = {'Result':'OK'}
 						return jsonify(result)		
 				
