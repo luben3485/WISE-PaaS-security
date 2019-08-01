ec6315b66caad0f939ffcc8604ddab1dcd4dc712
luben3485 <luben3485@gmail.com>
2019-08-02 02:20:57 +0800

    scan once for one person

--- zap-docker/web/app.py
+++ zap-docker/web/app.py
@@ -675,11 +675,19 @@ def cancelScan():
 def checkAnyScan():
 	try:
 		scans = db.listNotFinishedScans()
-		if len(scans) != 0:
+		if len(scans) > 0:
 			result = {'Result':'NO'}
 			return jsonify(result)
 		elif len(scans) == 0:
 			result = {'Result':'OK'}
+			return jsonify(result)
+		elif len(scans) == 1:
+			scanId = request.cookies.get('scanId')
+			for scan in scans:
+				if scan['scanId'] == scanId:
+					result = {'Result':'OK'}
+					return jsonify(result)
+			result = {'Result':'NO'}
 			return jsonify(result)
 
 	except Exception as err:
